3933014ce57e42408eab4f9dedc7a8fafbab8e1b
AC89 <82317889+AC899@users.noreply.github.com>
2022-06-22 23:00:00 +0100

    initial commit(2)

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 from deep_translator import GoogleTranslator
 from terminaltables import AsciiTable
+from time import sleep
 
 #Option to save the translation as a file
-#Error message if file path is incorrect & if lanmguage choice is not valid
 
 def main():
     while True:
-        table_data = [
-            ['Translate your file into:'],
-            ['French'],
-            ['German'],
-            ['Dutch'],
-            ['Romanian'],
-        ]
+        try:
+            table_data = [
+                ['Translate your file into:'],
+                ['French'],
+                ['German'],
+                ['Dutch'],
+                ['Romanian'],
+            ]
 
-        table = AsciiTable(table_data)
-        print(table.table)
-        p2 = input("Enter the language you want to translate your file into i.e. 'French'")
-        p = input("Enter the full file path i.e. Users/Desktop/File1: ")
-        path = p.replace('"','').replace("'","")
-        translated = GoogleTranslator(source='english', target=p2).translate_file(path)
-        print(translated)
+            table = AsciiTable(table_data)
+            print(table.table)
+            p2 = input("Enter the language you want to translate your file into i.e. 'French'")
+            p = input("Enter the full file path i.e. Users/Desktop/File1: ")
+            path = p.replace('"','').replace("'","")
+            translated = GoogleTranslator(source='english', target=p2).translate_file(path)
+            print(translated)
+        except:
+            print("Either the file path does not exist or you entered the language correctly. \n check the table and try again")
+
+
 
 if __name__ == '__main__':
     main()
